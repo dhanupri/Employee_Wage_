5a4fb4687a11b1c450d66b726901dc0b0abdecbf
unknown <dhanupriasokan@gmail.com>
2023-11-22 16:48:00 +0530

    usecase1

--- Employee.java
+++ Employee.java
@@ -2,9 +2,16 @@ import java.util.*;
 
 public class Employee {
 
+  public static int presentOrAnsent(){
+        int ans= (int) ((Math.random()*10)%3);
+        return ans;
+
+    }
+
 public static void main(String[] args){
        
 
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
+	int present_or_absent=presentOrAnsent();
 }
 }
