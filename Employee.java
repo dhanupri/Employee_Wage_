6cc84e48cae777e56816bac0d53e44c3d4c030b3
unknown <dhanupriasokan@gmail.com>
2023-11-22 19:04:05 +0530

    usecase5

--- Employee.java
+++ Employee.java
@@ -1,21 +1,18 @@
 import java.util.*;
-
-public class Employee {
- static  int Hour_wage=20;
+public class EmployeeWageComputation {
+    static  int Hour_wage=20;
     static int full_day_hour=8;
     static int part_time_hour=2;
-static int max_working_hour=100;
+    static int max_working_hour=100;
     static int max_woking_days=20;
     static int working_day_per_month=20;
 
-
-  public static int presentOrAnsent(){
+    public static int presentOrAnsent(){
         int ans= (int) ((Math.random()*10)%3);
         return ans;
 
     }
-
-   public static int calculateDailyWage(int present_or_absent){
+    public static int calculateDailyWage(int present_or_absent){
 
         int daily_wage;
         switch (present_or_absent){
@@ -33,7 +30,7 @@ static int max_working_hour=100;
         return daily_wage;
 
     }
- public static int calculateWork_hours(int present_or_Absent){
+    public static int calculateWork_hours(int present_or_Absent){
         switch (present_or_Absent){
             case 1:
                 return full_day_hour;
@@ -43,21 +40,18 @@ static int max_working_hour=100;
                 return 0;
         }
     }
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage Computation");
 
-public static void main(String[] args){
-       
-
-        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
-	 int monthlyWage=0;
-	int totworkingHour=0;
+        int monthlyWage=0;
+        int totworkingHour=0;
         int totworkingDays=0;
 
-        while(totworkingHour<max_working_hour && totworkingDays<max_woking_days){
-	int present_or_absent=presentOrAnsent();
-
- 	int daily_wage=calculateDailyWage(present_or_absent);
-	monthlyWage+=daily_wage;
-	 totworkingHour+=calculateWork_hours(present_or_absent);
+        while(totworkingHour<max_working_hour || totworkingDays<max_woking_days){
+            int present_or_absent=presentOrAnsent();
+            int daily_wage=calculateDailyWage(present_or_absent);
+            monthlyWage+=daily_wage;
+            totworkingHour+=calculateWork_hours(present_or_absent);
             if( present_or_absent==1 || present_or_absent==2){
                 totworkingDays++;
             }
@@ -68,7 +62,10 @@ public static void main(String[] args){
             System.out.println("Total Working Days:"+totworkingDays);
 
         }
-	}
-	
-}
+
+
+
+
+    }
+
 }
