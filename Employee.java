aa977e41fc348c1c00c76fd1aa8fe6e7e6c53bea
unknown <dhanupriasokan@gmail.com>
2023-11-22 17:20:28 +0530

    usecase3

--- Employee.java
+++ Employee.java
@@ -4,6 +4,10 @@ public class Employee {
  static  int Hour_wage=20;
     static int full_day_hour=8;
     static int part_time_hour=2;
+static int max_working_hour=100;
+    static int max_woking_days=20;
+    static int working_day_per_month=20;
+
 
   public static int presentOrAnsent(){
         int ans= (int) ((Math.random()*10)%3);
@@ -29,17 +33,41 @@ public class Employee {
         return daily_wage;
 
     }
+ public static int calculateWork_hours(int present_or_Absent){
+        switch (present_or_Absent){
+            case 1:
+                return full_day_hour;
+            case 2:
+                return part_time_hour;
+            default:
+                return 0;
+        }
+    }
 
 public static void main(String[] args){
        
 
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
 	 int monthlyWage=0;
-	for(int i=0;i<20;i++){
+	int totworkingHour=0;
+        int totworkingDays=0;
+
+        while(totworkingHour<max_working_hour && totworkingDays<max_woking_days){
 	int present_or_absent=presentOrAnsent();
 
  	int daily_wage=calculateDailyWage(present_or_absent);
 	monthlyWage+=daily_wage;
+	 totworkingHour+=calculateWork_hours(present_or_absent);
+            if( present_or_absent==1 || present_or_absent==2){
+                totworkingDays++;
+            }
+
+
+            System.out.println("total Monthly Wage:"+monthlyWage);
+            System.out.println("Total Working Hour:"+totworkingHour);
+            System.out.println("Total Working Days:"+totworkingDays);
+
+        }
 	}
 	
 }
